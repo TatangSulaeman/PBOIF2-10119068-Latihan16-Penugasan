14335d99737a704d24f678d212fa62acd236d0fe
amtat <amtat@DESKTOP-3DL3458>
2020-10-17 17:55:06 +0700

    PBOIF2-10119068-Latihan16-Penugasan

---
+++ src/pboif2/pkg10119068/latihan16/penugasan/PBOIF210119068Latihan16Penugasan.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119068.latihan16.penugasan;
+
+/**
+ *
+ * @author amtat
+ * NAMA     : TATANG SULAEMAN
+ * KELAS    : PBOIF2
+ * NIM      : 10119068
+ * Deskripsi program    : Penugasan
+ */
+public class PBOIF210119068Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= a = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+    }
+    
+}
